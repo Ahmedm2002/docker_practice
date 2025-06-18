849a220fe49b222122d973425aefc60711db44f9
Ahmedm2002 <ahmedmujtaba0159@gmail.com>
2025-06-19 04:11:53 +0500

    Added docker-compose file

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ const express = require("express");
 
 const app = express();
 
+const port = process.env.PORT || 4000;
+
 app.get("/", (req, res) => {
   res
     .status(200)
-    .json({ message: "Hello World", env: "App running in Docker" });
+    .json({ message: "Hello World", status: `App running on port ${port}` });
 });
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log("Server running on port 5000");
 });
